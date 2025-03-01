3d9cb00490afa66533f56ca2e61de38b6b96458a
do-ald533 <doouglas.aldred533@gmail.com>
2025-03-01 17:50:30 -0300

    feat: init

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageReporters: ['json', 'lcov'],
+    coverageDirectory: './coverage',
+    roots: ['<rootDir>/tests/unit'], //'<rootDir>/tests/integration'
+    transform: {
+      'ˆ.+\\.tsx?$': [
+        '@swc/jest',
+        {
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+              decorators: true,
+            },
+            target: 'es2021',
+          },
+          sourceMaps: true,
+          module: {
+            type: 'es6',
+            noInterop: false,
+          },
+        },
+      ],
+    },
+    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    verbose: true,
+  };
